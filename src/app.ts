ef7c3822251dcacecbe290efcccad7a2bc91f66e
shubhamchaudhary81 <aanandchaudharyshu@gmail.com>
2026-07-07 07:54:45 +0545

    TS file build

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,31 @@ import express from 'express';
 //create instance of express
 const app = express();
 
+// using middleware
+app.use(express.json());
+
+//health route
+app.get("/", (req,res) => {
+    res.status(200).json({
+        message: "Server is up & running",
+        status: "success",
+        success: true,
+        data: null
+    })
+});
+
+//using routes
+
+//error routes
+app.use((req,res) => {
+    const message = `Cannot ${req.method} on ${req.path}`;
+
+    res.status(404).json({
+        message,
+        status: 'fail',
+        success: false,
+        data: "null"
+    })
+})
+
 export default app;
